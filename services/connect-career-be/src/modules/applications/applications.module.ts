db1b928d7a6a7ac8ee46e34f09943b33d2e0acd5
Lmt310104 <lmtoan311@gmail.com>
2025-10-05 17:34:49 +0700

    feat: implement applications module with CRUD operations, application entity, and related services and controllers for managing job applications

--- services/connect-career-be/src/modules/applications/applications.module.ts
+++ services/connect-career-be/src/modules/applications/applications.module.ts
@@ -1,4 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Application } from './domain/entities/application.entity';
+import { Interview } from './domain/entities/interview.entity';
+import { Offer } from './domain/entities/offer.entity';
+import { Job } from '../jobs/domain/entities/job.entity';
+import { User } from '../identity/domain/entities';
+import { CandidateProfile } from '../profile/domain/entities/candidate-profile.entity';
+import { CV } from '../cv-maker/domain/entities/cv.entity';
+import { ApplicationService } from './api/services/application.service';
+import { ApplicationController } from './api/controller/application.controller';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Application,
+      Interview,
+      Offer,
+      Job,
+      User,
+      CandidateProfile,
+      CV,
+    ]),
+  ],
+  controllers: [ApplicationController],
+  providers: [ApplicationService],
+  exports: [ApplicationService],
+})
 export class ApplicationsModule {}
